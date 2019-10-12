eb61842f2bbafb5101c55b26c13e8ad8613fa784
Leonardo <sleonardoaugusto@gmail.com>
2019-10-12 15:08:27 -0300

    Now palyers can attack! :muscle:

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -3,16 +3,34 @@ new Vue({
   data: {
     monster: {
       name: 'Demogorgon',
-      life: 20
+      life: 100,
+      maxAttack: 13,
+      minAttack: 9
     },
     hero: {
       name: 'Jiraya',
-      life: 50
+      life: 100,
+      maxAttack: 10,
+      minAttack: 7
     }
   },
   methods: {
     getBgColor(life) {
       return life > 20 ? 'green' : 'red'
+    },
+    heroAttack() {
+      const { maxAttack, minAttack } = this.hero
+      const attackValue = this.attackValue(maxAttack, minAttack)
+      this.monster.life -= this.monster.life >= attackValue ? attackValue : this.monster.life
+      this.monsterAttack()
+    },
+    monsterAttack() {
+      const { maxAttack, minAttack } = this.monster
+      const attackValue = this.attackValue(maxAttack, minAttack)
+      this.hero.life -= this.hero.life >= attackValue ? attackValue : this.hero.life
+    },
+    attackValue(max, min) {
+      return Math.trunc(Math.random() * (max - min) + 1)
     }
   }
 })
