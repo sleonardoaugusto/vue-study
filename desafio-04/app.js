4dc705ce9157fd5dad02368b262e307c01459125
Leonardo <sleonardoaugusto@gmail.com>
2019-10-13 18:51:51 -0300

    Attack and specialAttack optimized

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -9,12 +9,12 @@ new Vue({
       life: 100,
       maxAttack: 10,
       minAttack: 7,
-      specialAttack: 3
+      specialAttack: 4
     },
     monster: {
       name: 'Demogorgon',
       life: 100,
-      maxAttack: 13,
+      maxAttack: 12,
       minAttack: 9
     }
   },
@@ -24,23 +24,23 @@ new Vue({
       this.monster.life = 100
       this.game.started = true
     },
-    heroAttack(special = false) {
-      this.attack('hero', 'monster', special)
-      this.monsterAttack()
+    attack(special) {
+      this.hurt('hero', 'monster', special)
+      this.hurt('monster', 'hero', false)
     },
-    monsterAttack() {
-      this.attack('monster', 'hero')
-    },
-    attack(givenBy, takenBy, special) {
+    hurt(givenBy, takenBy, special) {
       const { life } = this[takenBy]
       const { maxAttack, minAttack } = this[givenBy]
       const attackValue = this.attackValue(maxAttack, minAttack, special)
       this[takenBy].life -= life >= attackValue ? attackValue : life
     },
     attackValue(max, min, special) {
-      const specialAttack = special ? this.hero.specialAttack : 0
+      const plus = special ? this.hero.specialAttack : 0
+      return this.getRandom(min + plus, max + plus)
+    },
+    getRandom(min, max) {
       const value = Math.random() * (max - min) + min
-      return Math.round(value + specialAttack)
+      return Math.round(value)
     }
   },
   computed: {
