269aca1273bbc71e15dac8dc540f193353a1886b
Leonardo <sleonardoaugusto@gmail.com>
2019-10-17 23:21:21 -0300

    Component "Contador" added

---
+++ chapter-07/micro-project/src/components/Contador.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="contador">
+    <span>{{ contador }}</span>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contador',
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.contador++
+    },
+    decrement() {
+      this.contador--
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
