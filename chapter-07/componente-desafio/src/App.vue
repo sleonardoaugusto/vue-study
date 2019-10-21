c8dcc440f3bdc3110bf6c010deb5c94cc4a1ca97
Leonardo <sleonardoaugusto@gmail.com>
2019-10-21 00:36:05 -0300

    Lesson #107 - Break App template to Components

--- chapter-07/componente-desafio/src/App.vue
+++ chapter-07/componente-desafio/src/App.vue
@@ -1,68 +1,29 @@
 <template>
-	<v-app>
-		<v-navigation-drawer app>
-			<v-toolbar flat>
-				<v-list>
-					<v-list-tile>
-						<v-list-tile-title class="title">
-							Menu
-						</v-list-tile-title>
-					</v-list-tile>
-				</v-list>
-			</v-toolbar>
+  <v-app>
 
-			<v-divider></v-divider>
+    <Menu/>
 
-			<v-list dense class="pt-0">
-				<v-list-tile v-for="item in itensMenu" :key="item.titulo">
-					<v-list-tile-action>
-						<v-icon>{{ item.icon }}</v-icon>
-					</v-list-tile-action>
-					<v-list-tile-content>
-						<v-list-tile-title>{{ item.titulo }}</v-list-tile-title>
-					</v-list-tile-content>
-				</v-list-tile>
-			</v-list>
-		</v-navigation-drawer>
+    <Toolbar/>
 
-		<v-toolbar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Desafio</span>
-				<span class="font-weight-light">Componente</span>
-			</v-toolbar-title>
-		</v-toolbar>
+    <Carousel/>
 
-		<v-content>
-			<v-carousel>
-				<v-carousel-item
-					v-for="(item,i) in fotos"
-					:key="i"
-					:src="item.src"></v-carousel-item>
-			</v-carousel>
-		</v-content>
+    <Footer/>
 
-		<v-footer class="pa-3" app>
-			<v-spacer></v-spacer>
-			<div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-		</v-footer>
-	</v-app>
+  </v-app>
 </template>
 
 <script>
+import Menu from '@/components/Menu'
+import Carousel from '@/components/Carousel'
+import Footer from '@/components/Footer'
+import Toolbar from '@/components/Toolbar'
+
 export default {
-	data() {
-		return {
-			itensMenu: [
-				{titulo: 'Início', icon: 'dashboard'},
-				{titulo: 'Sobre', icon: 'question_answer'}
-			],
-			fotos: [
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
-				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
-			]
-		}
-	}
+  components: {
+    Menu, Carousel, Footer, Toolbar
+  },
+  data() {
+    return {}
+  }
 }
 </script>
