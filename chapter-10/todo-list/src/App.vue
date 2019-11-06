dcf83ef2641a0e56dcb04fc4848fa448717d8fd6
Leonardo <sleonardoaugusto@gmail.com>
2019-11-05 22:26:47 -0300

    Chapter 10 - App added, creating cards

---
+++ chapter-10/todo-list/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>App</h1>
+    <app-input @submit="add"/>
+  </div>
+</template>
+
+<script>
+import Input from '@/components/Input'
+
+export default {
+  name: 'app',
+  components: {
+    'app-input': Input
+  },
+  data: () => ({
+    card: []
+  }),
+  methods: {
+    add(e) {
+      this.card.push({ title: e })
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  body {
+    display: flex;
+    justify-content: center;
+    background-color: #FEF9EF;
+  }
+</style>
---
+++ chapter-10/todo-list/src/components/Input.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="input">
+    <input type="text" @input="setValue" @keypress.enter="submit">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data: () => ({
+    value: ''
+  }),
+  methods: {
+    submit() {
+      this.$emit('submit', this.value)
+    },
+    setValue(e) {
+      this.value = e.target.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  input {
+    width: 240px;
+    padding: 4px 7px;
+    border: 2px solid #FE6D73;
+    border-radius: 5px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    outline: none;
+  }
+
+</style>
