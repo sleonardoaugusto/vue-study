1b79864245edaa550079ecb78a8354137e983975
Leonardo <sleonardoaugusto@gmail.com>
2019-11-08 00:29:25 -0300

    Chapter 10 - Button add card added

--- chapter-10/todo-list/src/components/Input.vue
+++ chapter-10/todo-list/src/components/Input.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="input">
     <input type="text" @input="setValue" @keypress.enter="submit">
+    <span class="plus" @click="submit">+</span>
   </div>
 </template>
 
@@ -23,13 +24,31 @@ export default {
 
 <style scoped>
 
+  #input {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
   input {
     width: 240px;
-    padding: 4px 7px;
+    padding: 0.3rem;
     border: 2px solid #FE6D73;
     border-radius: 5px;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     outline: none;
   }
+
+  .plus {
+    position: absolute;
+    border: 1px solid #FE6D73;
+    background-color: #FE6D73;
+    border-radius: 3px;
+    font-weight: 600;
+    color: #FFF;
+    cursor: pointer;
+    padding: 0.2rem 0.4rem;
+  }
+
 
 </style>
