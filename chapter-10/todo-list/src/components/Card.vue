84f2c4c85376d99b6029f6b3bfa33c9061b76753
Leonardo <sleonardoaugusto@gmail.com>
2019-11-09 01:32:31 -0300

    Chapter 10 - Card style adjustment

--- chapter-10/todo-list/src/components/Card.vue
+++ chapter-10/todo-list/src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="wrap row">
-    <div class="card wrap column">
-      <span @click="close" class="close">x</span>
+  <div class="card wrap column">
+    <span @click="close" class="close">x</span>
+    <div class="content">
       <p>{{ data.title }}</p>
     </div>
   </div>
@@ -29,23 +29,30 @@ export default {
   .card {
     display: flex;
     justify-content: center;
+    margin: 20px;
     background-color: #FFCB77;
     border: 2px solid #FFCB77;
     color: #FFF;
     font-weight: 600;
     border-radius: 10px;
-    padding: 10px 25px;
-    margin: 25px;
     max-width: 150px;
   }
 
   .close {
+    padding: 5px 5px 0;
+    height: 20px;
+    width: 20px;
     align-self: flex-end;
     cursor: pointer;
+    font-size: 1.2rem;
   }
 
   p {
     max-width: 100%;
     word-break: break-all;
+  }
+
+  .content {
+    padding: 5px 10px;
   }
 </style>
